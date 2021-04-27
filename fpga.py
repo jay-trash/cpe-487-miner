4453b59748ef927aa6a4b5857d1561370f953750
Tyler Wright <tw39440@gmail.com>
2021-04-27 13:50:11 -0400

    Update fpga.py to match USB serial ports
    make sure you do the same if you try running this at some point

--- fpga.py
+++ fpga.py
@@ -10,7 +10,7 @@ import sha
 # Baud rates linux seems to support:
 # 0 50 75 110 134 150 200 300 600 1200 1800 2400 4800 9600 19200 38400 57600 115200 230400 460800 576000 921600 1152000 1500000 3000000...
 def test(br):
-    ser = serial.Serial("/dev/ttyUSB0", br, timeout=.1)
+    ser = serial.Serial("/dev/ttyUSB1", br, timeout=.1)
     try:
         ser.read(1)
     except serial.serialutil.SerialException:
@@ -25,7 +25,7 @@ def test(br):
 
 class FPGAController(object):
     def __init__(self):
-        self.ser = serial.Serial("/dev/ttyUSB0", 115200, timeout=4)
+        self.ser = serial.Serial("/dev/ttyUSB1", 115200, timeout=4)
 
         t = threading.Thread(target=self.read_thread)
         t.setDaemon(True)
